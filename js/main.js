6db8234848b9ff3ba8cfda24f251c8119cfc0ae7
Shamil <Shamil@Shamils-MacBook-Pro.local>
2020-05-14 13:07:54 -0500

    feat: connected jquery and used it

--- js/main.js
+++ js/main.js
@@ -1,26 +1,37 @@
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBnt = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+// document.addEventListener("DOMContentLoaded", function (event) {
+//   const modal = document.querySelector('.modal');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBnt = document.querySelector('.modal__close');
+//   const switchModal = () => {
+//     modal.classList.toggle('modal--visible');
+//   }
+//   modalBtn.forEach(element => {
+//     element.addEventListener('click', switchModal);
+//   });
+
+//   closeBnt.addEventListener('click', switchModal);
+// });
+
+
+
+// document.addEventListener('keydown', function (event) {
+//   var modal = document.querySelector('.modal')
+//   if (event.key === 'Escape') {
+//     modal.style.display = 'none'
+//   }
+// })
+
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+    modalBtn = $('[data-toggle=modal]'),
+    closelBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
   });
-
-  closeBnt.addEventListener('click', switchModal);
+  closelBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
 });
-
-
-
-document.addEventListener('keydown', function (event) {
-  var modal = document.querySelector('.modal')
-  if (event.key === 'Escape') {
-    modal.style.display = 'none'
-  }
-})
-
-
-
