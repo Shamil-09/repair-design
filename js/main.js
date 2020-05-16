a6d0e6e338ddc5bdf99e5fe5f53b4d2f72de6428
Shamil <Shamil@Shamils-MacBook-Pro.local>
2020-05-16 15:06:18 -0500

    feat: add slider and made responsive to small display

--- js/main.js
+++ js/main.js
@@ -11,6 +11,26 @@ $(document).ready(function () {
   closelBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 30 + bullets.width() + 30)
+  bullets.css('left', prev.width() + 30)
+
 });
 
 // -------------------------------------------------------------------------
