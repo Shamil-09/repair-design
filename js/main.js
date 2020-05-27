fb44bddbb38714488f7d952cf476224c19b5c1f3
Shamil <Shamil@Shamils-MacBook-Pro.local>
2020-05-27 08:04:35 -0500

    feat: linked youtube video to website

--- js/main.js
+++ js/main.js
@@ -183,6 +183,26 @@ $(document).ready(function () {
   $('[type=tel]').mask('+7(000)00-00-000', { placeholder: "+7 (000) 000-00-00" });
 
 
+  // youtube link
+
+  var player;
+  $('.video__play').on('click', function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '465',
+      width: '100%',
+      videoId: 'RHzzLqJWqHs',
+      events: {
+        'onReady': videoPlay,
+
+      }
+    });
+  })
+
+  function videoPlay(event) {
+    event.target.playVideo();
+  }
+
+
 
   // яндекс карта
 
